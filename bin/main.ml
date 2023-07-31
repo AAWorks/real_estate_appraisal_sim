032415ff46df15d3a32d1ec7116fa6c13da3e9f8
AAWorks <axalonso12@gmail.com>
2023-07-31 17:24:33 +0000

    adjust main for commandline game run

--- bin/main.ml
+++ bin/main.ml
@@ -6,7 +6,7 @@ let command =
   Command.async
     ~summary:"summary"
     (let%map_open.Command () = return () in
-     fun () -> Storage.pull_data ())
+     fun () -> Commandline.run ())
 ;;
 
 let () = Command_unix.run command
